51a9666e08436db0cb48cbc580e9691aaeefc2e7
jszvitor <jszvitor@gmail.com>
2020-06-13 19:18:05 -0300

    Added dynamic ProfileCard.

--- src/components/ProfileCard/index.js
+++ src/components/ProfileCard/index.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
+import githubApi from '../../services/githubApi';
+
 
 /* Styles */
 import './styles.css';
 
 class ProfileCard extends Component {
+  state = {
+    data: [],
+  }
+
+  async componentDidMount() {
+    const response = await githubApi.get(`/users/jszvitor`);
+    const data = response.data;
+
+    this.setState({ data });
+  };
+  
   render() {
+    const user = this.state.data;
+
     return (
       <div className="card">
         <img src="https://avatars3.githubusercontent.com/u/47458489?v=4" alt="avatar"/>
         <div className="card-body">
           <h1 className="name">
-            <span>José Vitor</span>
-            <span id="user-name">jszvitor</span>
+            <span>{user.name}</span>
+            <span>{user.login}</span>
           </h1>
-          <p className="description">Web developer full stack 👨🏻‍💻 #ReactJS #NodeJS</p>
+          <p className="description">{user.bio}</p>
         </div>
       </div>
     )
